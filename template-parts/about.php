6afa789bb1c2071c314c7f53a464f5064e1886f2
HuitziDomingo <huitzitzilidomingo@gmail.com>
2023-09-18 20:48:31 -0600

    separacion por componentes

---
+++ template-parts/about.php
@@ -0,0 +1,16 @@
+<section class="cont-about">
+    <div class="cont-about__text">
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>
+            Donec condimentum eros placerat, tincidunt orci nec, dignissim sem.
+            Aliquam ultrices sollicitudin ullamcorper. Donec tincidunt quis velit ac accumsan.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+        </p>
+
+        <a href="#" class="btn-primary">Conócenos</a>
+
+    </div>
+    <picture class="cont-about__img">
+        <img src="<?= get_template_directory_uri() ?>/build/images/sectionabout.webp" />
+    </picture>
+</section>
