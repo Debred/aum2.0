e4e761d29f3e7544aced5b8679c87a4248af6c19
HuitziDomingo <false>
2023-09-26 23:15:34 -0600

    slider home

--- template-parts/slide.php
+++ template-parts/slide.php
@@ -1,18 +1,30 @@
 <div class="cont-slider">
-    <h3 class="txt-center">Maestros Fundadores</h3>
+  <h3 class="txt-center">Maestros Fundadores</h3>
 
-    <!-- Swiper -->
+  <!-- Swiper -->
   <div class="swiper mySwiper">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">Slide 1</div>
-      <div class="swiper-slide">Slide 2</div>
-      <div class="swiper-slide">Slide 3</div>
-      <div class="swiper-slide">Slide 4</div>
-      <div class="swiper-slide">Slide 5</div>
-      <div class="swiper-slide">Slide 6</div>
-      <div class="swiper-slide">Slide 7</div>
-      <div class="swiper-slide">Slide 8</div>
-      <div class="swiper-slide">Slide 9</div>
+      <div class="swiper-slide">
+        <div class="cont-cards">
+          <img src="<?= get_template_directory_uri() ?>/build/images/domingodiasporta.webp" />
+          <h4>V. Sat Arhat</h4>
+          <h5>Domingo Dias Porta</h5>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <div class="cont-cards">
+          <img src="<?= get_template_directory_uri() ?>/build/images/maestreestrada.webp" />
+          <h4>V. Sat Arhat</h4>
+          <h5>Domingo Dias Porta</h5>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <div class="cont-cards">
+          <img src="<?= get_template_directory_uri() ?>/build/images/domingodiasporta.webp" />
+          <h4>V. Sat Arhat</h4>
+          <h5>Domingo Dias Porta</h5>
+        </div>
+      </div>
     </div>
     <div class="swiper-button-next"></div>
     <div class="swiper-button-prev"></div>
