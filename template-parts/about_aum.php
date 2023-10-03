3e2e001d7b9eadfc7d555edcafa0dba0e6c00588
HuitziDomingo <false>
2023-10-03 15:44:26 -0600

    aum about component

--- template-parts/about_aum.php
+++ template-parts/about_aum.php
@@ -1,52 +1,62 @@
 <div class="cont-about-aum">
     <h2 class="txt-center">Más sobre a.u.m.</h2>
 
-    <div class="cont-about-aum__cont__grid">
-        <div class="cont__cards">
-            <div class="bg-img">
-                <div class="bg-screen">
-                    <h3 class="txt-center">Acerca de AUM</h3>
-                    <p class="txt-center">
-                        Conoce acerca de esta organización sin fines de lucro.
-                        Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
-                    </p>
-                    <a href="#">leer más</a>
+    <div class="container">
+        <div class="cont-about-aum__cont__grid">
+            <div class="cont__cards">
+                <div class="bg-img">
+                    <div class="bg-screen">
+                        <div class="border-blue">
+                            <h3 class="txt-center">Acerca de AUM</h3>
+                            <p class="txt-center">
+                                Conoce acerca de esta organización sin fines de lucro.
+                                Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
+                            </p>
+                            <a href="#">leer más</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="cont__cards">
-            <div class="bg-img">
-                <div class="bg-screen">
-                    <h3 class="txt-center">Acerca de AUM</h3>
-                    <p class="txt-center">
-                        Conoce acerca de esta organización sin fines de lucro.
-                        Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
-                    </p>
-                    <a href="#">leer más</a>
+            <div class="cont__cards">
+                <div class="bg-img">
+                    <div class="bg-screen">
+                        <div class="border-blue">
+                            <h3 class="txt-center">Acerca de AUM</h3>
+                            <p class="txt-center">
+                                Conoce acerca de esta organización sin fines de lucro.
+                                Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
+                            </p>
+                            <a href="#">leer más</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="cont__cards">
-            <div class="bg-img">
-                <div class="bg-screen">
-                    <h3 class="txt-center">Acerca de AUM</h3>
-                    <p class="txt-center">
-                        Conoce acerca de esta organización sin fines de lucro.
-                        Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
-                    </p>
-                    <a href="#">leer más</a>
+            <div class="cont__cards">
+                <div class="bg-img">
+                    <div class="bg-screen">
+                        <div class="border-blue">
+                            <h3 class="txt-center">Acerca de AUM</h3>
+                            <p class="txt-center">
+                                Conoce acerca de esta organización sin fines de lucro.
+                                Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
+                            </p>
+                            <a href="#">leer más</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="cont__cards">
-            <div class="bg-img">
-                <div class="bg-screen">
-                    <h3 class="txt-center">Acerca de AUM</h3>
-                    <p class="txt-center">
-                        Conoce acerca de esta organización sin fines de lucro.
-                        Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
-                    </p>
-                    <a href="#">leer más</a>
+            <div class="cont__cards">
+                <div class="bg-img">
+                    <div class="bg-screen">
+                        <div class="border-blue">
+                            <h3 class="txt-center">Acerca de AUM</h3>
+                            <p class="txt-center">
+                                Conoce acerca de esta organización sin fines de lucro.
+                                Únete a las actividades que impulsan el desarrollo humano integral individual y de grupo.
+                            </p>
+                            <a href="#">leer más</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
