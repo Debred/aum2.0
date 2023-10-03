2b2ed0f099f9ae8d39460c7c89afa174f150faee
HuitziDomingo <false>
2023-10-02 21:45:11 -0600

    conf paginas nuevas

---
+++ page-templates/template-maestros.php
@@ -0,0 +1,15 @@
+<?php
+/*Template Name: Template de maestros*/
+?>
+
+
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+get_header();
+?>
+
+<?php get_template_part('../template-parts/cover')  ?>
+
+<?php get_footer() ?>
--- template-parts/cover.php
+++ template-parts/cover.php
@@ -1,3 +1,7 @@
+<?php if(is_front_page()): ?>
 <div class="cover">
     <h1 class="cover__title">A.U.M </br> Acción por la Unidad Mundial</h1>
 </div>
+<?php  elseif(is_page_template('../page-templates/tamplate-maestros.php')): ?>
+<p>hola</p>
+<?php  endif; ?>
