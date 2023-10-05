936ea008a5870805a4cc3a696eb0a3263f8d88ee
HuitziDomingo <false>
2023-10-04 19:52:09 -0600

    inicio de la pagina de acerca de

--- page-templates/template-maestros.php
+++ page-templates/template-maestros.php
@@ -10,4 +10,19 @@ get_header();
 
 <?php get_template_part('template-parts/cover') ?>
 
+<div class="container cont-page-about">
+    <h3 class="section-title titleblue">
+        Curabitur blandit tempus porttitor.
+        Nullam quis risus eget urna mollis ornare vel eu leo.
+        Nullam id dolor id nibh ultricies vehicula ut id elit.
+    </h3>
+    <p class="formatted-text">
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+    <h3 class="section-title titleyellow">
+        Sub-heading
+    </h3>
+</div>
+
 <?php get_footer() ?>
