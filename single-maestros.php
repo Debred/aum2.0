8ae68eb56ba0f6253a71261420bf3e5883021d35
Arturo <art.valverde.563@gmail.com>
2023-10-21 01:30:37 -0600

    Se añadió la template del single del CPT de maestros, también se hicieron cambios en los estilos globales

---
+++ single-maestros.php
@@ -0,0 +1,45 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+get_header();
+
+$thumbnail_id = get_post_thumbnail_id($post->ID);
+$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+?>
+
+<?php get_template_part('./template-parts/cover') ?>
+
+<div class="container page maestro">
+
+    <div class="heading">
+        <h2 class="title"><?= get_field('titulo') ?></h2>
+        <h1 class="name"><?= get_field('nombre') ?></h1>
+    </div>
+
+    <div class="maestro__image">
+        <?php if (has_post_thumbnail()) : ?>
+            <img src="<?= get_the_post_thumbnail_url() ?>" alt="<?= $thumbnail_id ?>" />
+        <?php endif; ?>
+    </div>
+
+    <main class="maestro__bio formatted-text">
+        <?php if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+
+                the_content();
+            }
+        } ?>
+    </main>
+
+    <aside class="maestro__estudios formatted-text">
+        <h3 class="title">Estudios: </h3>
+        <?= get_field('estudios') ?>
+    </aside>
+
+</div>
+
+<?php get_template_part('template-parts/slide')  ?>
+
+<?php get_footer() ?>
--- template-parts/cover.php
+++ template-parts/cover.php
@@ -2,6 +2,7 @@
     <section class="cover">
         <h1 class="cover__title h1">A.U.M </br> Acción por la Unidad Mundial</h1>
     </section>
+    
 <?php elseif (is_page()) : ?>
     <section class="cover">
         <h1 class="cover__title h1"><?= get_the_title(); ?></h1>
