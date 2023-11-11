0aaf41e7d5de10e96152a546b34a5f6e4a4e9504
HuitziDomingo <false>
2023-11-11 12:06:34 -0600

    add code

--- page-templates/template-links.php
+++ page-templates/template-links.php
@@ -10,8 +10,49 @@ get_header();
 
 <?php get_template_part('template-parts/cover') ?>
 <div class="container page with-sidebar locations-container">
-    <main class="bg-vi">
-        hola links
+    <main class="main-links">
+        <p class="parr-title">
+            Nos dice el M. S. MAESTRE Dr. Serge Raynaud de la Ferriere “Altruistas, filántropos,libres pensadores, idealistas, benefactores, artistas, científicos, filósofos, creyentes yno creyentes, educadores, maestros, constructores libres; todos los que anhelan ytrabajan por un mundo mejor, esperamos su colaboración, tal es: que unifiquemosnuestros esfuerzos, coordinándolos con el Centro, con el Eje Directriz Milenario delDestino Espiritual del Planeta.”
+        </p>
+
+        <div class="cont-links">
+            <div class="cont-links-sons">
+                <h3 class="section-title">
+                    SOBERNA ORDEN SOLAR DE CHICHAN ITZAAB
+                </h3>
+                <p>
+                    Venerable Maestro Domingo Días Porta Guía y Custodio
+                </p>
+                <a href="#">domingo.diasporta@gmail.com</a>
+                <a href="#">www.amasenda.com</a>
+                <p>Venerable Maestro Carlos Moisés Michán Amiga Rector de S.O.S.C.</p>
+                <a href="#">www.amasenda.com</a>
+            </div>
+            <div class="cont-links-sons">
+                <h3 class="section-title">
+                    SOBERNA ORDEN SOLAR DE CHICHAN ITZAAB
+                </h3>
+                <p>
+                    Venerable Maestro Domingo Días Porta Guía y Custodio
+                </p>
+                <a href="#">domingo.diasporta@gmail.com</a>
+                <a href="#">www.amasenda.com</a>
+                <p>Venerable Maestro Carlos Moisés Michán Amiga Rector de S.O.S.C.</p>
+                <a href="#">www.amasenda.com</a>
+            </div>
+            <div class="cont-links-sons">
+                <h3 class="section-title">
+                    SOBERNA ORDEN SOLAR DE CHICHAN ITZAAB
+                </h3>
+                <p>
+                    Venerable Maestro Domingo Días Porta Guía y Custodio
+                </p>
+                <a href="#">domingo.diasporta@gmail.com</a>
+                <a href="#">www.amasenda.com</a>
+                <p>Venerable Maestro Carlos Moisés Michán Amiga Rector de S.O.S.C.</p>
+                <a href="#">www.amasenda.com</a>
+            </div>
+        </div>
     </main>
     <?php get_sidebar(); ?>
 </div>
